ce343ce85a97326d990c95924dac75003dcd6f62
Sunrose Shrestha <sush19cs@cmrit.ac.in>
2022-12-11 16:37:24 +0530

    newcommit11122022

--- src/components/layout.js
+++ src/components/layout.js
@@ -70,7 +70,7 @@ const Layout = (props) => {
 
                     <input type='button' value={'0'} className='button number' onClick={handleClick}></input>
                     <input type='button' value={'.'} className='button number' onClick={handleClick}></input>
-                    <input type='button' value={'='} className='button operator' onClick={handleClick}></input>
+                    <input type='button' value={'='} className='equal-sign' onClick={handleClick}></input>
 
 
                 </div>
